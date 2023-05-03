9c671363c4b4a63f80bc1ad224bac2709824d730
bruno <bruno_am_22@hotmail.com>
2023-05-03 07:49:09 -0300

    ACTUALIZADO

--- be-src/index.ts
+++ be-src/index.ts
@@ -24,13 +24,14 @@ import {
    getPetToken,
    getAllPetCerca,
 } from "./controllers/pet-controllers";
-const port = 3000;
+
+const ruta = path.resolve(__dirname, "../dist");
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: "200mb" }));
+app.use(express.json({ limit: "50mb" }));
 const secrect = process.env.SECRECT;
-const ruta = path.resolve(__dirname, "../dist");
 app.get("/test", (req, res) => {
    console.log(process.env.PORT);
 
@@ -213,9 +214,9 @@ app.get("/pet-cerca-de", async (req, res) => {
    const respuesta = await getAllPetCerca(lat, lng);
    res.json([respuesta]);
 });
-app.get("/*", express.static(ruta));
-app.get("/*", function (req, res) {
-   res.sendFile(ruta + "/index.html");
+app.use(express.static(ruta));
+app.get("*", function (req, res) {
+   res.sendFile(ruta + "./index.html");
 });
 app.listen(port, () => {
    console.log("http://localhost:" + port);
