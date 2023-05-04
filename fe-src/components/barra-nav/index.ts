23301bd2580e65c5253a11daf360bf26708f5fd5
bruno <bruno_am_22@hotmail.com>
2023-05-04 17:24:52 -0300

    Actualizado

--- fe-src/components/barra-nav/index.ts
+++ fe-src/components/barra-nav/index.ts
@@ -73,7 +73,7 @@ class Barra extends HTMLElement {
       <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
          <div class="navbar-brand">
             <a class="navbar-item" href="#">
-               <img src=${logo}" height="28">
+               <img src="${logo}" height="28">
             </a>
          
             <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
