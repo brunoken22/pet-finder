d7f848b8d73e286739108140524f985aea409a53
brunoken22 <63690891+brunoken22@users.noreply.github.com>
2023-06-26 12:51:09 -0300

    Update sendinblue.ts

--- be-src/lib/sendinblue.ts
+++ be-src/lib/sendinblue.ts
@@ -2,8 +2,7 @@ const axios = require("axios");
 
 // Configura tus credenciales y detalles del correo electrónico
 export function sendinblue(data) {
-   const apiKey =
-      "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";
+   const apiKey =process.env.SENDINBLUE;
    const senderEmail = "bruno.am.59@gmail.com";
    const recipientEmail = data.nombreRecib;
    const subject = `${data.namePet} fue visto/a`;
